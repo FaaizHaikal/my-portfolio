38efc9644ed451e60b828ff28b565be00e0943f2
FaaizHaikal <faaizhaikal@gmail.com>
2024-12-29 02:30:01 +0700

    refactor: navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,88 +1,121 @@
 import { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 
 function Navbar() {
-  const navItems = ['Home', 'About', 'Experience', 'Projects', 'Contact'];
+  const navItems = ['About', 'Experiences', 'Projects', 'Contacts'];
 
   const theme = useTheme();
-  const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const handleDrawerToggle = () => {
-    setDrawerOpen(!drawerOpen);
+  const navigateTo = (item: string) => () => {
+    const id = '#' + item;
+    const element = document.querySelector(id);
+
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    } else {
+      console.error(`Element with id ${id} not found`);
+    }
   };
 
-  const sideBar = (
-    <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerToggle}>
-      <List>
-        {navItems.map((item, index) => (
-          <ListItem key={index} onClick={handleDrawerToggle}>
-            <ListItemText primary={item} />
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
-  );
-
   return (
-    <Box
+    <AppBar
+      position="static"
       sx={{
-        display: 'flex',
-        flexGrow: 1,
+        backgroundColor: 'rgb(22, 22, 22)',
       }}
     >
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: 'rgb(22, 22, 22)',
-          padding: isLargeScreen ? '50px' : '10px',
-        }}
-      >
-        <Toolbar
+      <Toolbar>
+        {/* Large screen navbar */}
+        <Box
           sx={{
-            justifyContent: isLargeScreen ? 'center' : 'flex-start',
+            display: isSmallScreen ? 'none' : 'flex',
+            width: '100%',
+            justifyContent: 'center',
           }}
         >
-          {!isLargeScreen && (
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={handleDrawerToggle}
+          {navItems.map((item) => (
+            <Typography
+              key={item}
+              variant="h6"
+              sx={{
+                margin: '0 30px',
+                padding: '0 10px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                ':hover': {
+                  backgroundColor: 'rgb(255, 255, 255)',
+                  color: 'rgb(22, 22, 22)',
+                },
+              }}
             >
-              <MenuIcon />
-            </IconButton>
-          )}
-          {isLargeScreen &&
-            navItems.map((item, index) => (
-              <Typography
-                key={index}
-                variant={isLargeScreen ? 'h1' : 'h6'}
-                component="div"
-                sx={{
-                  padding: isLargeScreen ? '0 100px' : '0 20px',
-                  cursor: 'pointer',
-                }}
-              >
-                {item}
-              </Typography>
-            ))}
-        </Toolbar>
-      </AppBar>
-      {!isLargeScreen && sideBar}
-    </Box>
+              {item}
+            </Typography>
+          ))}
+        </Box>
+
+        {/* Small screen navbar */}
+        <Box
+          sx={{
+            display: isSmallScreen ? 'flex' : 'none',
+            width: '100%',
+          }}
+        >
+          <IconButton onClick={() => setIsDrawerOpen(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor="left"
+            open={isDrawerOpen}
+            onClose={() => setIsDrawerOpen(false)}
+            sx={{
+              '& .MuiDrawer-paper': {
+                backgroundColor: 'rgb(22, 22, 22)',
+                color: 'rgb(255, 255, 255)',
+              },
+            }}
+          >
+            <List>
+              {navItems.map((item) => (
+                <ListItem
+                  sx={{
+                    margin: '10px 0',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    textAlign: 'center',
+                    cursor: 'pointer',
+                    ':hover': {
+                      backgroundColor: 'rgb(255, 255, 255)',
+                      color: 'rgb(22, 22, 22)',
+                    },
+                  }}
+                  key={item}
+                  onClick={navigateTo(item)}
+                >
+                  <ListItemText primary={item} />
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 
