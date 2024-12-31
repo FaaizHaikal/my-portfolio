7fd627a09d52607aacf52a97bb1077993752f81d
FaaizHaikal <faaizhaikal@gmail.com>
2024-12-31 17:47:59 +0700

    feat: update active link based on active section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,36 @@
+import { useEffect } from 'react';
 import './Header.css';
 
 function Navbar() {
+  useEffect(() => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
+
+    window.addEventListener('scroll', () => {
+      let current = '';
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+          const sectionId = section.getAttribute('id');
+          if (sectionId) {
+            current = sectionId;
+          }
+        }
+      });
+
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+        if ((link as HTMLAnchorElement).href.includes(current)) {
+          link.classList.add('active');
+        }
+      });
+    });
+  }, []);
+
+
   return (
     <header>
       <a href="#about" className="logo">
